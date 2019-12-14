c29fc8a4f2fc9960f12d1dc36e285598db65482a
miyanqii <shuhei.miyaki@smartdrive.co.jp>
2019-12-14 16:18:44 +0900

    implement CameraFragment

--- app/src/main/java/jp/miyanqii/cameraxwithobjectdetectionhandson/CameraFragment.kt
+++ app/src/main/java/jp/miyanqii/cameraxwithobjectdetectionhandson/CameraFragment.kt
@@ -1,7 +1,27 @@
 package jp.miyanqii.cameraxwithobjectdetectionhandson
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CameraFragment : Fragment() {
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!PermissionsFragment.hasPermissions(requireContext())) {
+            (activity as? MainActivity)?.moveToPermission()
+        }
+    }
 }
+
